c10cd4536e276ba9bb1da14db51c92e1c7e81646
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-20 14:00:41 -0400

    created route path so Get * returns the index.html page

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -6,4 +6,9 @@ const router = express.Router();
 const path = require(path);
 
 //created route so Get /notes will return notes.html files
-router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')))
+router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+//created route path so Get * returns the index.html page
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+module.exports = router;
