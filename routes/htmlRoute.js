543b0056e1772936a68726a8be990a2cdf2d780b
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-20 14:08:01 -0400

    installed express and tested the htmlRoute.js file, there was and error in the const path and i fixed it

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,9 +1,10 @@
 //import express package
 const express = require('express');
+
 //to create a new router object
 const router = express.Router();
 
-const path = require(path);
+const path = require('path');
 
 //created route so Get /notes will return notes.html files
 router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
