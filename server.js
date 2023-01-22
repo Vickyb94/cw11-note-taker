f538a9f6836fff09b3edb386003c4e9215bb7a65
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-22 14:31:23 -0400

    link files in the public folder with static middleware

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 //initialize the app variable by setting it to the value of express()
 const app = express();
-
 const path = require('path');
-
 //load server
 const PORT = process.env.PORT || 3001;
+
+//link files in the public folder with static middleware
+app.use(express.static('public'));
