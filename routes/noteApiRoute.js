206f0e4f89357f39127b41c332d91612eb821af0
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-22 17:41:41 -0400

    made changes to the path of db.json in the noteApiRoute.js

--- routes/noteApiRoute.js
+++ routes/noteApiRoute.js
@@ -4,11 +4,11 @@ const uuid = require('uuid');    //to create an id for each note
 const path = require('path')
 const fs = require('fs');
 
-const noteList = JSON.parse(fs.readFileSync('../db/db.json'));
+const noteList = JSON.parse(fs.readFileSync('db/db.json'));
 
 //create routes so Get /api/notes reads the db.json file and return all saved notes as JSON.
 router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'))
+    res.sendFile(path.join(__dirname, 'db/db.json'))
 });
 
 //post request sends a new note that is saved in json file and is returned to client
@@ -17,6 +17,7 @@ router.post('/api/notes', (req, res) => {res.json(noteList)
     //create title, text, and id for each note saved
     const noteInfo = {title: req.body.title, text: req.body, id: uuid.v9()};
       noteList.push(noteInfo);
+      fs.writeFileSync('db/db.json', JSON.stringify(db));
       res.json(noteList);
     });     
 
