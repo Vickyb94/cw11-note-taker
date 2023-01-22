d4ad360fb6d15aeb80330999abe3667141206b17
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-22 14:28:21 -0400

    created a const to load the server

--- server.js
+++ server.js
@@ -2,3 +2,8 @@
 const express = require('express');
 //initialize the app variable by setting it to the value of express()
 const app = express();
+
+const path = require('path');
+
+//load server
+const PORT = process.env.PORT || 3001;
