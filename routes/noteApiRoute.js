f26bdd92c5c01c1114523f18663a9982022c75c5
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-01-23 09:55:16 -0400

    write code to delete saved notes using params id

--- routes/noteApiRoute.js
+++ routes/noteApiRoute.js
@@ -15,13 +15,22 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {res.json(noteList)
     
     //create title, text, and id for each note saved
-    //const noteInfo = {title: req.body.title, text: req.body, id: uuid()};
+   
     const noteInfo = {title: req.body.title, text: req.body.text, id: uuid.v1};
       noteList.push(noteInfo);
-      //fs.writeFileSync('db/db.json', JSON.stringify(db));
       fs.writeFileSync('db/db.json', JSON.stringify(noteList));
       res.json(noteList);
-    });     
+    });   
+    
+    //to delete notes, a query parameter that contains id to be deleted will be sent
+    router.delete('/notes/:id', (req, res) => {
+      //filtering saved notes from the output and removing notes with id
+      const removeNote = noteList.filter(item => item.id !== req.params.id);
+      fs.writeFileSync('db/db.json', JSON.stringify(removeNote));
+      res.json(noteList);
+    });
+
+   
 
     module.exports = router;
 
